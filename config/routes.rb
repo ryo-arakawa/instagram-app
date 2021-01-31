a7b9d804c88502243af0422fc499c3059b9abacb
Ryo Arakawa <67402912+ryo-arakawa@users.noreply.github.com>
2021-01-31 14:53:14 +0900

    コメント機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
 
   get '/users/:id', to: 'users#show', as: 'user'
 
-  resources :posts, only: %i(index new create show destroy) do
+    resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
-
     resources :likes, only: %i(create destroy)
+    resources :comments, only: %i(create destroy)
+
   end
 end
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
